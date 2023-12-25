ea4f0dc426ac3ea66bde9f9b18c427dc737982a1
Shruti Vats <shrutivats.0503@gmail.com>
2023-12-25 23:14:26 +0530

    enough for the day

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+//NUMBER DATA TYPE
+ let data = 8             //number
+
+ let num = 2397474683468595754876433 
+ console.log(num) // gives result 2.3974746834685957e+24
+
+ //to get exact number use datatype bigint 
+ let number = 2397474683468595754876433n
+console.log(number)
+//just add 'n' in end & its a bigint 
+//result 2397474683468595754876433n
+
+//to add number to bigint
+console.log(number+2n)
+
+//STRING DATATYPE
+let user = "Shr\vuti \"Vats\""
+console.log(user) 
+// "\v"= vertical tab
---
+++ helloworld.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+//variable
+
+console.log(2+2) //without variable
+let num = 2+2.5 //use let for the first time for declaration
+console.log(num)
+
+let user = "Shruti"
+console.log(user)
+
+//constants
+let r = 5
+const pi = 3.14 //const used for making a variable constant
+let area = pi*r*r
+console.log(area)
