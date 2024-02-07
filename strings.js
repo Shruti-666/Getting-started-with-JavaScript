9b540b3cfd8b94b6e2cbf323dc8114703493c61d
Shruti Vats <shrutivats.0503@gmail.com>
2024-02-07 20:50:36 +0530

    Stating Strings

---
+++ strings.js
@@ -0,0 +1,11 @@
+var name1 = "Shruti Vats"
+var channel = "shruti.com"
+// var temp = `${name1} have a channel named ${channel}`
+console.log(`${name1} have a channel named ${channel}`);
+//to get length
+var len = name1.length;
+console.log(`Length of name is ${len}`);
+console.log("Hello world\nShruti\tVats")
+//string constructor
+ var y = new String("Shruti")
+ console.log(y);
