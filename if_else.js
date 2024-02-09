01bd32187487a4716d21363868cacf14c81856d6
Shruti Vats <shrutivats.0503@gmail.com>
2024-02-09 20:07:00 +0530

    still going

---
+++ if_else.js
@@ -0,0 +1,12 @@
+const age = 12;
+if(age>=18){
+    console.log(`You can vote`)
+}
+else{
+    console.log("You cannot vote")
+}
+
+//ternary operator
+const marks =50;
+const result =(marks>40)?"PASSED":"FAIL"
+console.log(result);
