005899830d859079304caa39640a5dfc5c119722
Shruti Vats <shrutivats.0503@gmail.com>
2024-05-23 15:55:56 +0530

    Function

---
+++ functions.js
@@ -0,0 +1,10 @@
+//basics of function
+//makes code DRY(dont repeat yourself)
+function sum (a,b){
+    const sum = a+b;
+    console.log("The result is ",sum);
+}
+
+sum(4,3);
+sum(12,4);
+sum(2,24);
--- loop.js
+++ loop.js
@@ -1,4 +1,5 @@
 // three loop for, while, do while
 for(let index =0; index < 5; index++){
-    console.log("Hello world");
+    console.log("Hello world", index);
 }
+
