4c504a50e53637c7c9c32691ba65e3a4bdd1dac9
Shruti Vats <92461435+Shruti-666@users.noreply.github.com>
2024-05-28 07:41:54 +0000

    datastpes

---
+++ caj/datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as new version
+// alert("hello") --we are using nodejs not browser
+
+//number 
+//bigint
+//string = ""
+//boolean true fa
+//null -- standalone value -- representaion of empty value
+//undefined 
+//symbol -- unique
+
+//object
+console.log(typeof undefined);
