17c2c803b124345449a5090d660cd6cdbc59831f
Shruti Vats <92461435+Shruti-666@users.noreply.github.com>
2024-05-28 07:20:07 +0000

    variables

---
+++ caj/test.js
@@ -0,0 +1,15 @@
+//not changable
+const accountId =3059; 
+
+
+let accountEmail = "shrutivats.2003@gmail.com"
+var accountpassword ="12345" //dont use
+accountCity = "Delhi"
+
+//accountId=2 //not allowed
+
+accountEmail ="abc.com"
+accountpassword ="3059"
+accountCity = "chd"
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountCity])
